aa0779087578bf5678ec131ccb4f394a91808e9d
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-12-07 09:07:11 +0700

    create base

---
+++ src/app/house/house-delete/house-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-house-delete',
+  templateUrl: './house-delete.component.html',
+  styleUrls: ['./house-delete.component.css']
+})
+export class HouseDeleteComponent {
+
+}
