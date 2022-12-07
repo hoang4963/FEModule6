64c4684c9ba42917767d5deebc5cf88e266a7b5c
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-12-07 09:23:10 +0700

    model of house

--- src/app/model/house.ts
+++ src/app/model/house.ts
@@ -1,2 +1,12 @@
 export interface House {
+  id?: number;
+  houseName?: string;
+  houseAddress?: string;
+  bedrooms?: number;
+  bathrooms?: number;
+  rent?: number;
+  description?: string;
+  user?: any;
+  status?: any;
+  image?: any;
 }
