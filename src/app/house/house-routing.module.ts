8136c6a2d2f177a61e4175d6b0079982b0c4d2dd
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-12-08 11:57:04 +0700

    housecrud

--- src/app/house/house-routing.module.ts
+++ src/app/house/house-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HouseListComponent} from "./house-list/house-list.component";
-
-<<<<<<< HEAD
 import {HouseDetailComponent} from "./house-detail/house-detail.component";
-=======
->>>>>>> 069071bee071085006ba4cd28d72a5afacbcfb37
-
 
 const routes: Routes = [{
   path: 'list',
