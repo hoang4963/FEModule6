2bfa2cb1429d587510c5eed7db81aa936fb64fd5
quanph14 <phquan.0493@gmail.com>
2022-12-08 10:08:48 +0700

    done register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   {
     path: 'house',
     loadChildren: () => import('./house/house.module').then(module => module.HouseModule)
+  },{
+    path: 'register',
+    component: RegisterComponent
   }
+
 ];
 
 @NgModule({
