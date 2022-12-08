e4a2dd98f7c6bf6a06e6c057aa32e19a11cabe94
hoang4963 <hoangth.finbox@gmail.com>
2022-12-08 09:53:49 +0700

    a

--- src/app/house/house-create/house-create.component.ts
+++ src/app/house/house-create/house-create.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {HouseService} from "../../service/house.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-house-create',
@@ -7,9 +9,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HouseCreateComponent implements OnInit{
 
-  constructor() {
+  constructor(private houseService: HouseService) {
   }
   ngOnInit(): void {
   }
-
+  houseForm: FormGroup = new FormGroup({
+      houseName: new FormControl(),
+      houseAddress: new FormControl(),
+      bedrooms: new FormControl(),
+      bathrooms: new FormControl(),
+      rent: new FormControl(),
+      description: new FormControl(),
+      status: new FormControl(),
+      image1: new FormControl(),
+      image2: new FormControl(),
+      image3: new FormControl(),
+  })
 }
