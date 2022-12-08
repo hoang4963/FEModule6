ab828731bfdcdfb1d94bc41fa85d4480bb9d0c8e
Duonghieu97st <Duonghieu97st@gmail.com>
2022-12-08 15:01:39 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import {RegisterComponent} from "./register/register.component";
-=======
 import {LoginComponent} from "./jwt/login/login.component";
->>>>>>> 84bb63555e734128748f29f4e8da0c048845644b
 
 const routes: Routes = [
   {
     path: 'house',
     loadChildren: () => import('./house/house.module').then(module => module.HouseModule)
-<<<<<<< HEAD
   },{
     path: 'register',
     component: RegisterComponent
-=======
   },
   {
     path: '',
     component: LoginComponent
->>>>>>> 84bb63555e734128748f29f4e8da0c048845644b
   }
 
 ];
