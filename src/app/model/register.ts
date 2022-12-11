bf464cac71d74740e829e2869d8f80e1d6001b0f
quanph14 <phquan.0493@gmail.com>
2022-12-11 13:47:03 +0700

    fix register

--- src/app/model/register.ts
+++ src/app/model/register.ts
@@ -1,6 +1,12 @@
-export interface Register {
+export class Register {
   userName?: string;
   password?: string;
   email?: string;
+  confirmPassword?: string;
   phone?: string;
+  constructor(userName: string, phone: string, password: string) {
+    this.userName = userName;
+    this.phone = phone;
+    this.password = password;
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {Register} from "../model/register";
 
 @Component({
   selector: 'app-register',
@@ -7,36 +10,32 @@ import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "
   // styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  title = 'reactive-form';
-  contactForm = new FormGroup({
-    userName: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.email,Validators.required]),
-    password: new FormControl('', [Validators.required,Validators.minLength(6)]),
-    confirmPassword: new FormControl('', [Validators.required,Validators.minLength(6)]),
-    phone: new FormControl('',[Validators.required,Validators.pattern("(84|0[3|5|7|8|9])+([0-9]{8})\\b")]),
-  },{validators:this.validateAreEqual});
-  get userName(){
-    return this.contactForm.get('userName');
-  }
-  get password(){
-    return this.contactForm.get('password');
-  }
-  get confirmPassword(){
-    return this.contactForm.get('confirmPassword');
-  }
-  get phone(){
-    return this.contactForm.get('phone');
-  }
-  get email(){
-    return this.contactForm.get('email');
-  }
 
 
-  onSubmit() {
-    console.log(this.contactForm.value);
-  }
+  signupForm! : FormGroup;
+  constructor(private formBuilder: FormBuilder, private http : HttpClient,
+              private router : Router) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      userName:['',Validators.required, Validators.minLength(6)],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword:['',Validators.required, Validators.minLength(6)],
+      phone:['',Validators.required, Validators.pattern("(84|0[3|5|7|8|9])+([0-9]{8})\\b")],
+
+    },{validators:this.validateAreEqual})
+  }
+  signUp(){
+    this.http.post<Register>("http://localhost:8080/register",this.signupForm.value)
+      .subscribe(res=>{
+        alert("Đăng ký tài khoản thành công");
+        this.signupForm.reset();
+        this.router.navigate(['login']);
+      },err=>{
+        console.log(err);
+        alert("Tài khoản đã tồn tại. Vui lòng đăng ký lại");
+      })
+
   }
   public validateAreEqual(c: AbstractControl): {notSame: boolean} | null {
     return  c.value.password  ===  c.value.confirmPassword ? null : {notSame: true};
