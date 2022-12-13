6eeb0fae7a815de343692d37614dc408a2bbe35c
quanph14 <phquan.0493@gmail.com>
2022-12-13 16:52:25 +0700

    hien thi rating 90%

--- src/app/house/house-detail/house-detail.component.ts
+++ src/app/house/house-detail/house-detail.component.ts
@@ -5,6 +5,8 @@ import {HouseService} from "../../service/house.service";
 import {Image} from "../../model/Image";
 import {HouseCommentService} from "../../service/house-comment.service";
 import {Comments} from "../../model/comment";
+import {Rating} from "../../model/rating";
+import {HouseRatingService} from "../../service/house-rating.service";
 
 @Component({
   selector: 'app-house-detail',
@@ -27,18 +29,20 @@ export class HouseDetailComponent {
   bedrooms! : any;
   bathrooms! : any;
   listComment: Comments[]=[];
-
+  listRating: Rating[]=[];
   constructor(private houseService: HouseService,
               private router: Router,
               private activatedRoute: ActivatedRoute,
-              private houseCommentService: HouseCommentService) {
+              private houseCommentService: HouseCommentService,
+              private houseRatingService: HouseRatingService) {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       // @ts-ignore
       this.id = +paramMap.get('id');
       this.getHouse(this.id);
       this.initializeForm();
       this.getImage(this.id);
-      this.getComment(this.id)
+      this.getComment(this.id);
+      this.getRating(this.id)
     });
   }
 
@@ -91,6 +95,12 @@ export class HouseDetailComponent {
   getComment(id: number){
     return this.houseCommentService.getAll().subscribe(commentList => {
       this.listComment = commentList;
+      console.log(this.listComment)
+    } )
+  }
+  getRating(id: number){
+    return this.houseRatingService.getAll().subscribe(ratingList => {
+      this.listRating = ratingList;
       console.log(this.listComment)
     } )
   }
